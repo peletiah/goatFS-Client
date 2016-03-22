8b374aecb797d63ce111e6f703016b3f2a727d66
Christian Benke <benkokakao@gmail.com>
2016-03-22 19:56:47 +0100

    stashy stash

--- scripts/main.js
+++ scripts/main.js
@@ -11,38 +11,53 @@ import Administration from './components/Administration';
 import NotFound from './components/NotFound';
 
 /*
-  Routes
+  Main
 */
 
-
-function requireAuth(nextState, replace) {
-  console.log('Testing auth-status')
-  console.log(App.loggedIn())
-  if (!App.loggedIn()) {
-    console.log('No csrfToken found, asking for login')
-    replace({
-      pathname: '/login',
-      state: {nextPathname: nextState.location.pathname }
-    })
+  
+  function requireAuth(nextState, replace) {
+    console.log('Testing auth-status')
+    console.log(App.loggedIn())
+    if (!App.loggedIn()) {
+      console.log('No csrfToken found, asking for login')
+      replace({
+        pathname: '/login',
+        state: {nextPathname: nextState.location.pathname }
+      })
+    }
   }
-}
-
-
-function createHello(Hello, props) {
-  return <Hello log={App.state.log} fetchLog={App.fetchLog} />
-}
-
-render((
-  <Router history={hashHistory}>
-    <Router createElement={createHello}/>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="/login" component={LoginForm}/>
-      <Route path="/hello" component={Hello}/>
-      <Route path="/routing" component={Routing}/>
-      <Route path="/administration" component={Administration}/>
-      <Route path="*" component={NotFound}/>
-    </Route>
-  </Router>
-
-), document.querySelector('#main'))
+  
+  function fetchLog() {
+    console.log('Fetching Log from server')
+    fetch('http://localhost:6543/hello', {
+      credentials: 'include',
+      headers: {
+        'X-CSRF-TOKEN': this.state.csrfToken
+      }
+    }).then(r => r.json())
+      .then(data => this.setState({
+          log : data
+      }, console.log(data)))
+      .catch(e => console.log("Error"))
+    }
+  
+  
+  
+  function createHello(Hello, props) {
+    return <Hello log={App.state.log} fetchLog={fetchLog()} />
+  }
+  
+  render((
+    <Router history={hashHistory}>
+      <Router createElement={createHello}/>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home}/>
+        <Route path="/login" component={LoginForm}/>
+        <Route path="/hello" component={Hello}/>
+        <Route path="/routing" component={Routing}/>
+        <Route path="/administration" component={Administration}/>
+        <Route path="*" component={NotFound}/>
+      </Route>
+    </Router>
+  
+  ), document.querySelector('#main'))
